de375cc51880fca3c169f1782baf7b0932763f9d
Isaac Sudweeks <147561760+isaa-sudweeks@users.noreply.github.com>
2026-01-19 14:41:36 -0700

    feat: Implement finite difference gradient operators and reflection boundary conditions for the 2D wave solver.

--- Solvers/2D_Wave/Fixed_Point_Arbitrary_Percision/JaxSolver.py
+++ Solvers/2D_Wave/Fixed_Point_Arbitrary_Percision/JaxSolver.py
@@ -88,7 +88,169 @@ def make_fixed_point_ops(ubound, lbound):
         L=L,
         dtype=dtype
     )
-# ... (gradients are fine) ...
+def make_grad_x(fp, idx_by_12, idx_by_2):
+    @jax.jit
+    def grad_x(u):
+        # Interior: 2:-2
+        # -u[4:, :] + 8*u[3:-1, :] - 8*u[1:-3, :] + u[:-4, :]
+        t1 = -u[4:, :]
+        t2 = u[3:-1, :] * fp.to_fixed(8)
+        t3 = u[1:-3, :] * fp.to_fixed(8)
+        t4 = u[:-4, :]
+        
+        centered = t1 + t2 - t3 + t4
+        res_interior = centered * idx_by_12
+        
+        # Edges
+        # 0: (-3 u0 + 4 u1 - u2) / 2dx
+        row0 = (u[0, :] * fp.to_fixed(-3) + u[1, :] * fp.to_fixed(4) - u[2, :]) * idx_by_2
+        row1 = (-u[0, :] + u[2, :]) * idx_by_2
+        row_m2 = (-u[-3, :] + u[-1, :]) * idx_by_2
+        row_m1 = (u[-3, :] - u[-2, :] * fp.to_fixed(4) + u[-1, :] * fp.to_fixed(3)) * idx_by_2
+        
+        # Assemble
+        # We can use .at[...].set(...) which returns a new BigIntTensor
+        
+        # Start with zeros
+        out = u * fp.to_fixed(0) # Zero tensor matching u
+        
+        out = out.at[2:-2, :].set(res_interior)
+        out = out.at[0, :].set(row0)
+        out = out.at[1, :].set(row1)
+        out = out.at[-2, :].set(row_m2)
+        out = out.at[-1, :].set(row_m1)
+        
+        return out
+    return grad_x
+
+def make_grad_y(fp, idy_by_12, idy_by_2):
+    @jax.jit
+    def grad_y(u):
+        # Interior: 2:-2
+        t1 = -u[:, 4:]
+        t2 = u[:, 3:-1] * fp.to_fixed(8)
+        t3 = u[:, 1:-3] * fp.to_fixed(8)
+        t4 = u[:, :-4]
+        
+        centered = t1 + t2 - t3 + t4
+        res_interior = centered * idy_by_12
+        
+        # Edges
+        col0 = (u[:, 0] * fp.to_fixed(-3) + u[:, 1] * fp.to_fixed(4) - u[:, 2]) * idy_by_2
+        col1 = (-u[:, 0] + u[:, 2]) * idy_by_2
+        col_m2 = (-u[:, -3] + u[:, -1]) * idy_by_2
+        col_m1 = (u[:, -3] - u[:, -2] * fp.to_fixed(4) + u[:, -1] * fp.to_fixed(3)) * idy_by_2
+        
+        out = u * fp.to_fixed(0)
+        out = out.at[:, 2:-2].set(res_interior)
+        out = out.at[:, 0].set(col0)
+        out = out.at[:, 1].set(col1)
+        out = out.at[:, -2].set(col_m2)
+        out = out.at[:, -1].set(col_m1)
+        
+        return out
+    return grad_y
+
+def make_grad_xx(fp, idx_sq, idx_sq_by_12):
+    @jax.jit
+    def grad_xx(u):
+        # -u[i+2] + 16u[i+1] - 30u[i] + 16u[i-1] - u[i-2]
+        t1 = -u[4:, :]
+        t2 = u[3:-1, :] * fp.to_fixed(16)
+        t3 = u[2:-2, :] * fp.to_fixed(30)
+        t4 = u[1:-3, :] * fp.to_fixed(16)
+        t5 = -u[:-4, :]
+        
+        centered = (t1 + t2 - t3 + t4 + t5) * idx_sq_by_12
+        
+        # Edges
+        # 0: 2u0 - 5u1 + 4u2 - u3
+        row0 = (u[0, :]*fp.to_fixed(2) - u[1, :]*fp.to_fixed(5) + u[2, :]*fp.to_fixed(4) - u[3, :]) * idx_sq
+        row1 = (u[0, :] - u[1, :]*fp.to_fixed(2) + u[2, :]) * idx_sq
+        row_m2 = (u[-3, :] - u[-2, :]*fp.to_fixed(2) + u[-1, :]) * idx_sq
+        row_m1 = (-u[-4, :] + u[-3, :]*fp.to_fixed(4) - u[-2, :]*fp.to_fixed(5) + u[-1, :]*fp.to_fixed(2)) * idx_sq
+        
+        out = u * fp.to_fixed(0)
+        out = out.at[2:-2, :].set(centered)
+        out = out.at[0, :].set(row0)
+        out = out.at[1, :].set(row1)
+        out = out.at[-2, :].set(row_m2)
+        out = out.at[-1, :].set(row_m1)
+        return out
+    return grad_xx
+
+def make_grad_yy(fp, idy_sq, idy_sq_by_12):
+    @jax.jit
+    def grad_yy(u):
+        t1 = -u[:, 4:]
+        t2 = u[:, 3:-1] * fp.to_fixed(16)
+        t3 = u[:, 2:-2] * fp.to_fixed(30)
+        t4 = u[:, 1:-3] * fp.to_fixed(16)
+        t5 = -u[:, :-4]
+        
+        centered = (t1 + t2 - t3 + t4 + t5) * idy_sq_by_12
+        
+        col0 = (u[:, 0]*fp.to_fixed(2) - u[:, 1]*fp.to_fixed(5) + u[:, 2]*fp.to_fixed(4) - u[:, 3]) * idy_sq
+        col1 = (u[:, 0] - u[:, 1]*fp.to_fixed(2) + u[:, 2]) * idy_sq
+        col_m2 = (u[:, -3] - u[:, -2]*fp.to_fixed(2) + u[:, -1]) * idy_sq
+        col_m1 = (-u[:, -4] + u[:, -3]*fp.to_fixed(4) - u[:, -2]*fp.to_fixed(5) + u[:, -1]*fp.to_fixed(2)) * idy_sq
+
+        out = u * fp.to_fixed(0)
+        out = out.at[:, 2:-2].set(centered)
+        out = out.at[:, 0].set(col0)
+        out = out.at[:, 1].set(col1)
+        out = out.at[:, -2].set(col_m2)
+        out = out.at[:, -1].set(col_m1)
+        return out
+    return grad_yy
+
+
+def make_reflect_fn(fp):
+    three = fp.to_fixed(3)
+    @jax.jit
+    def apply_reflect(u_stack):
+        # u_stack is shape (2, Nx, Ny, Limbs) effectively, but generalized by BigIntTensor
+        # BigIntTensor wrapping (2, Nx, Ny, L)
+        # Slicing dim 0
+        phi = u_stack[0]
+        chi = u_stack[1]
+        
+        zero = fp.to_fixed(0)
+        
+        # Dirichlet on phi
+        phi = phi.at[0, :].set(zero)
+        phi = phi.at[-1, :].set(zero)
+        phi = phi.at[:, 0].set(zero)
+        phi = phi.at[:, -1].set(zero)
+        
+        # Neumann on chi: 4u_1 - u_2 ?? 
+        # Original: (4*chi[1] - chi[2]) / 3
+        # In fixed point: div((4*chi[1] - chi[2]), 3)
+        
+        def neumann(near, far):
+            val = near * fp.to_fixed(4) - far
+            return fp.fixed_div(val, three)
+            
+        chi = chi.at[0, :].set(neumann(chi[1, :], chi[2, :]))
+        chi = chi.at[-1, :].set(neumann(chi[-2, :], chi[-3, :]))
+        chi = chi.at[:, 0].set(neumann(chi[:, 1], chi[:, 2]))
+        chi = chi.at[:, -1].set(neumann(chi[:, -2], chi[:, -3]))
+        
+        # Stack back?
+        # BigIntTensor doesn't support jnp.stack directly if we pass items. 
+        # Better: use .at on the original u_stack.
+        
+        # Construct new u_stack?
+        # We can try to use jnp.stack on the underlying tensors and re-wrap, 
+        # assuming they have same shape/limbs.
+        # But u_stack[0] slice returns a BigIntTensor.
+        
+        # Let's use the underlying tensors for stacking.
+        # phi.tensor, chi.tensor
+        new_tensor = jnp.stack((phi.tensor, chi.tensor), axis=0)
+        return BigIntTensor(new_tensor, frac_bits=fp.frac_bits)
+        
+    return apply_reflect
 
 def make_rhs_fn(fp, grad_xx, grad_yy, inv_rsq_eps):
     @jax.jit
